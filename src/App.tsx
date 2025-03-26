d125fd74b42e49a5e0c6ec18d63edfc10f7b8c9f
Kilian Armas Pérez <kilamper@gmail.com>
2025-03-26 21:26:44 +0000

    feat: add Home page and update routing in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
+import Home from "./pages/Home";
 import Content from "./pages/Content";
 import Overview from "./pages/Overview";
 import Settings from "./pages/Settings";
@@ -10,7 +11,8 @@ const App = () => {
       <Routes>
         {/* Rutas dentro del Layout */}
         <Route path="/" element={<Layout />}>
-          <Route index element={<Content />} />
+          <Route index element={<Home />} />
+          <Route path="content" element={<Content />} />
           <Route path="overview" element={<Overview />} />
           <Route path="settings" element={<Settings />} />
         </Route>
